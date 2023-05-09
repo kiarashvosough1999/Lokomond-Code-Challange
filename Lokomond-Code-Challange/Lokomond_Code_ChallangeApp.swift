55ca6fb6e9dd6d303bb8f3bc629e68a0963499b3
Kiarash Vosough <vosough.k@gmail.com>
2023-05-09 12:16:21 +0330

    first commit

---
+++ Lokomond-Code-Challange/Lokomond_Code_ChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lokomond_Code_ChallangeApp.swift
+//  Lokomond-Code-Challange
+//
+//  Created by Kiarash Vosough on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Lokomond_Code_ChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Lokomond-Code-Challange
